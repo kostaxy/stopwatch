623b75542f9a02bc0465d44170d4af32f3c60f9d
kostaxy <kshandrak@yandex.by>
2022-03-14 20:51:34 +0300

    add lap time column

--- src/components/LapsList/LapsList.jsx
+++ src/components/LapsList/LapsList.jsx
@@ -15,24 +15,53 @@ const LapsList = ({ laps }) => {
         {
             title: 'lap',
             dataIndex: 'numLap',
+            align: 'center'
 
         },
         {
-            title: 'time',
-            dataIndex: 'time',
+            title: 'total time',
+            dataIndex: 'TotalTime',
+            align: 'center'
+        },
+        {
+            title: 'lap time',
+            dataIndex: 'LapTime',
+            align: 'center'
         }
     ];
 
     const adaptLap = (laps) => {
         let lapsObjArr = []
         for (let i = 0; i < laps.length; i++) {
-            lapsObjArr.push(
-                {
-                    'key': i,
-                    'numLap': '#' + (i + 1),
-                    'time': ('00' + laps[i].minutes).slice(-2) + ':' + ('00' + laps[i].seconds).slice(-2) + ':' + ('000' + laps[i].milliseconds).slice(-3)
-                }
-            )
+            //первый круг
+            if (i === 0) {
+                lapsObjArr.push(
+                    {
+                        'key': i,
+                        'numLap': '#' + (i + 1),
+                        'TotalTime': ('00' + laps[i].minutes).slice(-2) + ':' + ('00' + laps[i].seconds).slice(-2) + ':' + ('000' + laps[i].milliseconds).slice(-3),
+                        'LapTime': ('00' + laps[i].minutes).slice(-2) + ':' + ('00' + laps[i].seconds).slice(-2) + ':' + ('000' + laps[i].milliseconds).slice(-3)
+                    }
+                )
+            } else {
+                let lapFullTimeMilliseconds = (laps[i].milliseconds + laps[i].seconds * 1000 + laps[i].minutes * 1000 * 60) - (laps[i - 1].milliseconds + laps[i - 1].seconds * 1000 + laps[i - 1].minutes * 1000 * 60)
+
+                // sec = sec % 60
+                // ms = ms % 1000
+                let lapSec = Math.floor(lapFullTimeMilliseconds / 1000)
+                let lapMin = Math.floor(lapSec / 60)
+                lapSec = lapSec % 60
+                let lapMs = lapFullTimeMilliseconds - (lapMin * 60 * 1000) - (lapSec * 1000)
+                lapsObjArr.push(
+                    {
+                        'key': i,
+                        'numLap': '#' + (i + 1),
+                        'TotalTime': ('00' + laps[i].minutes).slice(-2) + ':' + ('00' + laps[i].seconds).slice(-2) + ':' + ('000' + laps[i].milliseconds).slice(-3),
+                        'LapTime': ('00' + lapMin).slice(-2) + ':' + ('00' + lapSec).slice(-2) + ':' + ('000' + lapMs).slice(-3)
+                    }
+                )
+            }
+
         }
         lapsObjArr.reverse()
         return lapsObjArr
@@ -48,14 +77,18 @@ const LapsList = ({ laps }) => {
 
     return (
         <div className={classes.LapsTab}>
-            <SimpleBar style={{ maxHeight: calculateHeightLapsTab() }}>
+            <SimpleBar
+                style={{ maxHeight: calculateHeightLapsTab() }}
+            >
                 <Table
                     columns={columns}
                     dataSource={data}
                     pagination={false}
                 // scroll={{ y: calculateHeightLapsTab() }}
+                // scroll={{ y:  'calc(100vh - 500px)' }}
                 />
             </SimpleBar>
+
         </div>
     )
 };
