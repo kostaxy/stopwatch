e740e7dcd826ab74be01a2d9d6ccc11ffd1b7677
shandrak <shandrak@ktc.rw>
2022-03-28 11:52:01 +0300

    update About page

--- src/components/pages/About/About.jsx
+++ src/components/pages/About/About.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React from 'react';
+import classes from './About.module.css';
+import { FaGithubSquare, FaLinkedin } from 'react-icons/fa';
 
 const About = () => {
   return (
-    <div>@kostaxy</div>
+    <div className={classes.About__wrapper}>
+      <div className={classes.About_header}>
+        Stopwatch app
+      </div>
+      <div className={classes.About_content}>
+        This web application was created by
+        <a href='https://github.com/kostaxy'> @kostaxy</a>.
+        The main goals were training to work with time and react.
+        <br />The project was created using react, antd libraries.
+      </div>
+      <hr className={classes.Contact_line} />
+    
+      <div className={classes.About_contacts}>
+        <a href='https://www.linkedin.com/in/shandrak/'>
+          <FaLinkedin size={32} />
+        </a>
+        <a href='https://github.com/kostaxy'>
+          <FaGithubSquare size={32} />
+        </a>
+      </div>
+    </div >
   )
 }
 
